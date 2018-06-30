f4f6f9eecfedbc436ffb4008376c1855e7de105a
Hassan Zaki <hassan.zaki@badrit.com>
2018-06-30 13:53:40 +0200

    Move classifier url to configurations

--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -2,7 +2,8 @@ require 'net/http'
 
 class NotificationService < BaseService
 
-  SERVICE_URL = "http://localhost:3001/notify" 
+  #TODO HZ: Move to configuration
+  SERVICE_URL = "http://localhost:3001/notify"
 
   def initialize(time_slots_url, fields_url)
     @time_slots_url = time_slots_url
@@ -10,7 +11,7 @@ class NotificationService < BaseService
   end
 
   def call
-    uri = URI(SERVICE_URL)
+    uri = URI(Rails.application.config.classifier_service_url)
     Net::HTTP.post_form(uri, { time_slots_url: @time_slots_url, fields_url: @fields_url })
   rescue
     #TODO HZ: Notify Admin?
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.classifier_service_url = "http://localhost:3001/notify"
 
   Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 end
